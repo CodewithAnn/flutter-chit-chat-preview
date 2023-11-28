583676ea5c08d1fd6b78afdeda18a01cabecb4e2
Anand jha <codewithann@gmail.com>
2023-11-28 23:32:27 +0530

    added app color

--- lib/Theams/colors/colors.dart
+++ lib/Theams/colors/colors.dart
@@ -1,3 +1,9 @@
 import 'package:flutter/material.dart';
 
-const lightBlue = Color(0xff5271ff);
+const lightNavy = Color(0xff5271ff); // main color
+const blueWhite = Color(0xffCDD6FF); // use with lightNavy for gradient.
+const lightBlue = Color(0xff7DBEFF);
+const vividNavy = Color(0xff123CFF); // accent color
+const baseNavy = Color(0xff91A5FF); // base color
+const onBackground = Colors.white;
+const darkGrey = Color(0xff485B6F);
