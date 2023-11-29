0563272482c894240bf5bdb4861d89c8d2a14ab4
Anand jha <codewithann@gmail.com>
2023-11-29 13:40:41 +0530

    update dark Theme

--- lib/Theme/dark_Theme.dart
+++ lib/Theme/dark_Theme.dart
@@ -2,17 +2,23 @@ import 'package:chit_chat/Theme/colors/colors.dart';
 import 'package:flutter/material.dart';
 
 var darkTheme = ThemeData(
-  useMaterial3: true,
-  colorScheme: const ColorScheme(
-      brightness: Brightness.dark,
-      background: Colors.black,
-      onBackground: darkGrey,
-      surface: Colors.teal,
-      onSurface: Colors.black87,
-      secondary: baseNavy,
-      onSecondary: Colors.black,
-      primary: lightNavy,
-      onPrimary: Colors.white,
-      error: Colors.orange,
-      onError: Colors.white),
-);
+    useMaterial3: true,
+    colorScheme: const ColorScheme(
+        brightness: Brightness.dark,
+        primary: black,
+        onPrimary: Colors.white,
+        background: Colors.black,
+        onBackground: darkGrey,
+        surface: lightBlue,
+        onSurface: Colors.black87,
+        secondary: baseNavy,
+        onSecondary: Colors.black,
+        error: Colors.orange,
+        onError: Colors.white),
+    appBarTheme:const AppBarTheme(
+      color: darkGrey,
+      foregroundColor: white,
+      centerTitle: true,
+      elevation: 4,
+      titleTextStyle: TextStyle(color: white,fontSize: 30,fontFamily: "Poppins BoldItalic",),
+    ));
