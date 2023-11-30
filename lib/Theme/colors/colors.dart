70dcea25b9fa5b07f2d2a747ce5956326faa879b
Anand jha <codewithann@gmail.com>
2023-12-01 00:41:50 +0530

    Update colors

--- lib/Theme/colors/colors.dart
+++ lib/Theme/colors/colors.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-const lightNavy = Color(0xff5271ff); // main color
+const lightNavy = Color(0xff5271ff); // main color light mode
 const blueWhite = Color(0xffCDD6FF); // use with lightNavy for gradient.
-const lightBlue = Color(0xff7DBEFF);
+const lightBlue = Color(0xff7DBEFF); // main color in dark mode.
+const transBlue = Color.fromARGB(255, 113, 180, 247);
 const vividNavy = Color(0xff123CFF); // accent color
 const baseNavy = Color(0xff91A5FF); // base color
 const onBackground = Colors.white;
