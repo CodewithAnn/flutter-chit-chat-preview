1aa844773ed74af368bce4cb9e5d518b6f2e46ed
Anand jha <codewithann@gmail.com>
2023-12-02 20:30:58 +0530

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'package:chit_chat/Screens/login_Screen.dart';
 import 'package:chit_chat/Theme/colors/colors.dart';
+import 'package:chit_chat/Theme/dark_Theme.dart';
+import 'package:chit_chat/Theme/light_Theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(body: 
-    Container(width: 50,
-    height: 70,color: lightBlue,),),
+    theme: lightTheme,
+    home: const MyApp(),
+    
   ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const LoginScreen();
+  }
 }
